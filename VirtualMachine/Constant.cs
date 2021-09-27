8948ad443a1583d858d68eb7fcadab57c20eb689
Gregory Sanchez <gsanchez@evopointsolution.com>
2021-09-27 12:30:08 -0400

    Add project files.

---
+++ VirtualMachine/Constant.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualMachine
+{
+    public static class Constant
+    {
+        public const string GROUP_NAME = "az204-resourceGroup";
+        public const string VIRTUAL_MACHINE = "az204-VMTesting";
+        public const string VNET_NAME = "az204VNET";
+        public const string VNET_ADDRESS = "172.16.0.0/16";
+        public const string SUBNET_NAME = "az204Subnet";
+        public const string SUBNET_ADDRESS = "127.16.0.0/24";
+        public const string NICNAME = "az204NIC";
+        public const string ADMIN_USER = "azureadmin";
+        public const string ADMIN_PASSWORD = "P@$$w0rd2021";
+                
+        public const string PUBLISHER = "MicrosoftWindowsServe";
+        public const string OFFER = "windowServer";
+        public const string SKU = "2012-R2-Datacenter";
+    }
+}
---
+++ VirtualMachine/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Azure.Management.Compute.Fluent.Models;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using System;
+
+namespace VirtualMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var credentials = SdkContext.AzureCredentialsFactory.FromFile("./azureauth.properties");
+
+            var azure = Azure.Configure()
+                .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
+                .Authenticate(credentials)
+                .WithDefaultSubscription();
+
+            var location = Region.USEast2;
+
+            Console.WriteLine($"Creating resource group {Constant.GROUP_NAME}...");
+            var resourceGroup = azure.ResourceGroups.Define(Constant.GROUP_NAME)
+                .WithRegion(location)
+                .Create();
+
+            //Every virtual machine needs to be connected to a virtual network.
+            Console.WriteLine($"Creating virtual network {Constant.VNET_NAME}...");
+            var network = azure.Networks.Define(Constant.VNET_NAME)
+                .WithRegion(location)
+                .WithExistingResourceGroup(Constant.GROUP_NAME)
+                .WithAddressSpace(Constant.VNET_ADDRESS)
+                .Create();
+
+            //Any virtual machine need a network interface for connecting to the virtual network
+            Console.WriteLine($"Creating network interface {Constant.NICNAME}...");
+            var nic = azure.NetworkInterfaces.Define(Constant.NICNAME)
+                .WithRegion(location)
+                .WithExistingResourceGroup(Constant.GROUP_NAME)
+                .WithExistingPrimaryNetwork(network)
+                .WithSubnet(Constant.SUBNET_NAME)
+                .WithPrimaryPrivateIPAddressDynamic()
+                .Create();
+
+            //create the virtual machine
+            Console.WriteLine($"Creating virtual machinee {Constant.VIRTUAL_MACHINE}...");
+            azure.VirtualMachines.Define(Constant.VIRTUAL_MACHINE)
+                .WithRegion(location)
+                .WithExistingResourceGroup(Constant.GROUP_NAME)
+                .WithExistingPrimaryNetworkInterface(nic)
+                .WithLatestWindowsImage(Constant.PUBLISHER, Constant.OFFER, Constant.SKU)
+                .WithAdminUsername(Constant.ADMIN_USER)
+                .WithAdminPassword(Constant.ADMIN_PASSWORD)
+                .WithComputerName(Constant.VIRTUAL_MACHINE)
+                .WithSize(VirtualMachineSizeTypes.StandardDS2V2)
+                .Create();
+        }
+    }
+}
